c7586cb3c976af5d090d45264d4846b911b0a358
Diego López León <dieguitoll@gmail.com>
2011-01-05 10:42:33 -0300

    JBRULES-2835: implementations for process abstract persistence

---
+++ jbpm-persistence-jpa/src/main/java/org/jbpm/persistence/MapBasedProcessPersistenceContext.java
@@ -0,0 +1,54 @@
+package org.jbpm.persistence;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.drools.persistence.map.MapBasedPersistenceContext;
+import org.jbpm.persistence.processinstance.ProcessInstanceInfo;
+
+public class MapBasedProcessPersistenceContext extends MapBasedPersistenceContext
+    implements
+    ProcessPersistenceContext,
+    NonTransactionalProcessPersistentSession{
+    
+    private ProcessStorage storage;
+    private Set<ProcessInstanceInfo> processes;
+
+    public MapBasedProcessPersistenceContext(ProcessStorage storage) {
+        super( storage );
+        this.storage = storage;
+        this.processes = new HashSet<ProcessInstanceInfo>();
+    }
+
+    public void persist(ProcessInstanceInfo processInstanceInfo) {
+        processes.add( processInstanceInfo );
+        if( processInstanceInfo.getId() == null ) {
+            processInstanceInfo.setId( storage.getNextProcessInstanceId() );
+        }
+    }
+
+    public ProcessInstanceInfo findProcessInstanceInfo(Long processId) {
+        return storage.findProcessInstanceInfo( processId );
+    }
+
+    public List<ProcessInstanceInfo> getStoredProcessInstances() {
+        return Collections.unmodifiableList( new ArrayList<ProcessInstanceInfo>(processes));
+    }
+
+    @Override
+    public void close() {
+        super.close();
+        processes.clear();
+    }
+
+    public void remove(ProcessInstanceInfo processInstanceInfo) {
+        storage.removeProcessInstanceInfo( processInstanceInfo.getId() );
+    }
+
+    public List<Long> getProcessInstancesWaitingForEvent(String type) {
+        return storage.getProcessInstancesWaitingForEvent( type );
+    }
+}
