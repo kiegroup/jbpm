75e259adce9451d7ba53023ffbc20ccf27145c35
Diego López León <dieguitoll@gmail.com>
2011-01-11 23:44:22 -0300

    JBRULES-2835: refactor test to support different backends

--- jbpm-persistence-jpa/src/test/java/org/jbpm/persistence/map/impl/MapPersistenceTest.java
+++ jbpm-persistence-jpa/src/test/java/org/jbpm/persistence/map/impl/MapPersistenceTest.java
@@ -1,32 +1,20 @@
 package org.jbpm.persistence.map.impl;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.drools.KnowledgeBase;
 import org.drools.KnowledgeBaseFactory;
 import org.drools.common.AbstractRuleBase;
 import org.drools.impl.InternalKnowledgeBase;
-import org.drools.persistence.info.SessionInfo;
-import org.drools.persistence.info.WorkItemInfo;
-import org.drools.persistence.jpa.JPAKnowledgeService;
-import org.drools.persistence.map.EnvironmentBuilder;
 import org.drools.process.instance.WorkItemHandler;
-import org.drools.runtime.Environment;
-import org.drools.runtime.EnvironmentName;
-import org.drools.runtime.KnowledgeSessionConfiguration;
 import org.drools.runtime.StatefulKnowledgeSession;
 import org.drools.runtime.process.WorkItem;
 import org.drools.runtime.process.WorkItemManager;
-import org.jbpm.persistence.ProcessStorage;
-import org.jbpm.persistence.ProcessStorageEnvironmentBuilder;
-import org.jbpm.persistence.processinstance.ProcessInstanceInfo;
+import org.jbpm.process.instance.ProcessInstance;
+import org.jbpm.ruleflow.core.RuleFlowProcess;
+import org.jbpm.ruleflow.instance.RuleFlowProcessInstance;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MapPersistenceTest {
+public abstract class MapPersistenceTest {
 
     @Test
     public void startProcessInPersistentEnvironment() {
@@ -36,75 +24,13 @@ public class MapPersistenceTest {
         ((AbstractRuleBase) ((InternalKnowledgeBase) kbase).getRuleBase())
                 .addProcess( ProcessCreatorForHelp.newShortestProcess( processId ) );
         
-        ProcessStorage storage = getSimpleProcessStorage();
-
-        StatefulKnowledgeSession crmPersistentSession = createSession(kbase, storage);
+        StatefulKnowledgeSession crmPersistentSession = createSession(kbase);
 
         crmPersistentSession.startProcess(processId);
 
         crmPersistentSession.dispose();
     }
 
-    private ProcessStorage getSimpleProcessStorage() {
-        return new ProcessStorage() {
-
-            private Map<Long, SessionInfo> ksessions = new HashMap<Long, SessionInfo>();
-            private Map<Long, ProcessInstanceInfo> processes = new HashMap<Long, ProcessInstanceInfo>();
-            private Map<Long, WorkItemInfo> workItems = new HashMap<Long, WorkItemInfo>();
-
-            public void saveOrUpdate(SessionInfo ksessionInfo) {
-                ksessionInfo.update();
-                ksessions.put( ksessionInfo.getId(), ksessionInfo );
-            }
-
-            public SessionInfo findSessionInfo(Long id) {
-                return ksessions.get( id );
-            }
-
-            public ProcessInstanceInfo findProcessInstanceInfo(Long processInstanceId) {
-                return processes.get( processInstanceId );
-            }
-
-            public void saveOrUpdate(ProcessInstanceInfo processInstanceInfo) {
-                processInstanceInfo.update();
-                processes.put( processInstanceInfo.getId(), processInstanceInfo );
-            }
-
-            public long getNextProcessInstanceId() {
-                return processes.size()+1;
-            }
-
-            public void removeProcessInstanceInfo(Long id) {
-                processes.remove( id );
-            }
-
-            public List<Long> getProcessInstancesWaitingForEvent(String type) {
-                List<Long> processInstancesWaitingForEvent = new ArrayList<Long>();
-                for ( ProcessInstanceInfo processInstanceInfo : processes.values() ) {
-                    if( processInstanceInfo.getEventTypes().contains( type ) )
-                        processInstancesWaitingForEvent.add( processInstanceInfo.getId() );
-                }
-                return processInstancesWaitingForEvent;
-            }
-
-            public void saveOrUpdate(WorkItemInfo workItemInfo) {
-                workItems.put( workItemInfo.getId(), workItemInfo );
-            }
-
-            public Long getNextWorkItemId() {
-                return new Long(workItems.size() + 1);
-            }
-
-            public WorkItemInfo findWorkItemInfo(Long id) {
-                return workItems.get( id );
-            }
-
-            public void remove(WorkItemInfo workItemInfo) {
-                workItems.remove( workItemInfo.getId() );
-            }
-        };
-    }
-    
     @Test
     public void createProcessStartItDisposeAndLoadItAgain() {
         String processId = "minimalProcess";
@@ -115,8 +41,7 @@ public class MapPersistenceTest {
                 .addProcess( ProcessCreatorForHelp.newProcessWithOneWork( processId,
                                                                           workName ) );
 
-        ProcessStorage storage = getSimpleProcessStorage();
-        StatefulKnowledgeSession crmPersistentSession = createSession(kbase, storage);
+        StatefulKnowledgeSession crmPersistentSession = createSession(kbase);
 
         DummyWorkItemHandler handler = new DummyWorkItemHandler();
         crmPersistentSession.getWorkItemManager()
@@ -124,7 +49,7 @@ public class MapPersistenceTest {
 
         long process1Id = crmPersistentSession.startProcess(processId).getId();
 
-        crmPersistentSession = disposeAndReloadSession(crmPersistentSession, kbase, storage);
+        crmPersistentSession = disposeAndReloadSession(crmPersistentSession, kbase);
         crmPersistentSession.getWorkItemManager().registerWorkItemHandler(workName, handler);
 
         long workItemId = handler.getLatestWorkItem().getId();
@@ -137,61 +62,58 @@ public class MapPersistenceTest {
 
     }
     
-    private StatefulKnowledgeSession createSession(KnowledgeBase kbase,
-                                                   ProcessStorage storage) {
-        
-        EnvironmentBuilder envBuilder = new ProcessStorageEnvironmentBuilder( storage );
-        Environment env = KnowledgeBaseFactory.newEnvironment();
-        //FIXME temporary usage of this constants
-        env.set( EnvironmentName.TRANSACTION_MANAGER,
-                 envBuilder.getTransactionManager() );
-        env.set( EnvironmentName.ENTITY_MANAGER_FACTORY,
-                 envBuilder.getPersistenceContextManager() );
+    @Test
+    public void signalEventTest() {
+        String processId = "signalProcessTest";
+        String eventType = "myEvent";
+        RuleFlowProcess process = ProcessCreatorForHelp.newSimpleEventProcess( processId,
+                                                         eventType );
 
-        return JPAKnowledgeService.newStatefulKnowledgeSession( kbase,
-                                                                null,
-                                                                env );
+        KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
+        ((AbstractRuleBase) ((InternalKnowledgeBase) kbase).getRuleBase()).addProcess( process );
+        
+        StatefulKnowledgeSession crmPersistentSession = createSession(kbase);
+
+        RuleFlowProcessInstance processInstance = (RuleFlowProcessInstance) crmPersistentSession.startProcess( processId );
+        long processInstanceId = processInstance.getId();
+        Assert.assertEquals( ProcessInstance.STATE_ACTIVE,
+                             processInstance.getState() );
+
+        crmPersistentSession = createSession(kbase);
+
+        crmPersistentSession.signalEvent( eventType,
+                              null );
+        processInstance = (RuleFlowProcessInstance) crmPersistentSession.getProcessInstance( processInstanceId );
+
+        Assert.assertNull( processInstance );
     }
 
-    private StatefulKnowledgeSession disposeAndReloadSession(StatefulKnowledgeSession ksession,
-                                                             KnowledgeBase kbase,
-                                                             ProcessStorage storage) {
-        long sessionId = ksession.getId();
-        ksession.dispose();
-        EnvironmentBuilder envBuilder = new ProcessStorageEnvironmentBuilder( storage );
-        Environment env = KnowledgeBaseFactory.newEnvironment();
-        //FIXME temporary usage of this constants
-        env.set( EnvironmentName.TRANSACTION_MANAGER,
-                 envBuilder.getTransactionManager() );
-        env.set( EnvironmentName.ENTITY_MANAGER_FACTORY,
-                 envBuilder.getPersistenceContextManager() );
-        
-        return JPAKnowledgeService.loadStatefulKnowledgeSession( sessionId, kbase, null, env );
-    }
-
+    protected abstract StatefulKnowledgeSession createSession(KnowledgeBase kbase);
+    
+    protected abstract StatefulKnowledgeSession disposeAndReloadSession(StatefulKnowledgeSession crmPersistentSession,
+                                                                        KnowledgeBase kbase);
+    
     private static class DummyWorkItemHandler
-    implements
-    WorkItemHandler {
+        implements
+        WorkItemHandler {
 
-    private WorkItem latestWorkItem;
+        private WorkItem latestWorkItem;
 
-    public void executeWorkItem(WorkItem workItem,
-                                WorkItemManager manager) {
-        this.setLatestWorkItem( workItem );
+        public void executeWorkItem(WorkItem workItem,
+                                    WorkItemManager manager) {
+            this.setLatestWorkItem( workItem );
+        }
+
+        public void abortWorkItem(WorkItem workItem,
+                                  WorkItemManager manager) {
+        }
+
+        public void setLatestWorkItem(WorkItem latestWorkItem) {
+            this.latestWorkItem = latestWorkItem;
+        }
+
+        public WorkItem getLatestWorkItem() {
+            return latestWorkItem;
+        }
     }
-
-    public void abortWorkItem(WorkItem workItem,
-                              WorkItemManager manager) {
-    }
-
-    public void setLatestWorkItem(WorkItem latestWorkItem) {
-        this.latestWorkItem = latestWorkItem;
-    }
-
-    public WorkItem getLatestWorkItem() {
-        return latestWorkItem;
-    }
-
-}
-
 }
