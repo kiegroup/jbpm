c698c3dc716f747522e36ec3e2338125fa9f470c
marianbuenosayres <marianbuenosayres@gmail.com>
2011-06-08 18:03:33 -0300

    Added undoable event to resize effect

--- jbpm-gwt/jbpm-gwt-form-builder/src/main/java/org/jbpm/formbuilder/client/effect/ResizeEffect.java
+++ jbpm-gwt/jbpm-gwt-form-builder/src/main/java/org/jbpm/formbuilder/client/effect/ResizeEffect.java
@@ -1,11 +1,18 @@
 package org.jbpm.formbuilder.client.effect;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jbpm.formbuilder.client.bus.UndoableEvent;
+import org.jbpm.formbuilder.client.bus.UndoableEventHandler;
 import org.jbpm.formbuilder.client.form.FBFormItem;
+import org.jbpm.formbuilder.client.resources.FormBuilderGlobals;
 import org.jbpm.formbuilder.client.resources.FormBuilderResources;
 import org.jbpm.formbuilder.common.handler.ResizeEvent;
 import org.jbpm.formbuilder.common.handler.ResizeEventHandler;
 import org.jbpm.formbuilder.common.panels.ResizablePanel;
 
+import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -32,9 +39,9 @@ public class ResizeEffect extends FBFormEffect {
         widgetWidth = item.getOffsetWidth() + 20;
         Widget actualWidget = getItem().getWidget();
         ResizablePanel resizable = new ResizablePanel(actualWidget, widgetWidth, widgetHeight);
-        resizable.addResizeHandler(new ResizeEventHandler() { //TODO make undoable
+        resizable.addResizeHandler(new ResizeEventHandler() {
             public void onResize(ResizeEvent event) {
-                getItem().setSize("" + event.getWidth() + "px", "" + event.getHeight() + "px");
+                undoableEvent(event, getItem());
                 getItem().clear();
                 getItem().setWidget(event.getWidget());
             }
@@ -42,5 +49,32 @@ public class ResizeEffect extends FBFormEffect {
         getItem().clear();
         getItem().setWidget(resizable);
         resizable.setSize("" + widgetWidth + "px", "" + widgetHeight + "px");
+    }
+    
+    protected void undoableEvent(ResizeEvent event, FBFormItem item) {
+        Map<String, Object> dataSnapshot = new HashMap<String, Object>();
+        EventBus bus = FormBuilderGlobals.getInstance().getEventBus();
+        dataSnapshot.put("item", item);
+        dataSnapshot.put("oldWidth", item.getWidth());
+        dataSnapshot.put("oldHeight", item.getHeight());
+        dataSnapshot.put("newWidth", "" + event.getWidth() + "px");
+        dataSnapshot.put("newHeight", "" + event.getHeight() + "px");
+        bus.fireEvent(new UndoableEvent(dataSnapshot, new UndoableEventHandler() {
+            public void onEvent(UndoableEvent event) { /* do nothing */ }
+            public void undoAction(UndoableEvent event) {
+                FBFormItem item = (FBFormItem) event.getData("item");
+                String oldWidth = (String) event.getData("oldWidth");
+                String oldHeight = (String) event.getData("oldHeight");
+                item.setSize(oldWidth, oldHeight);
+            }
+            public void doAction(UndoableEvent event) {
+                FBFormItem item = (FBFormItem) event.getData("item");
+                String newWidth = (String) event.getData("newWidth");
+                String newHeight = (String) event.getData("newHeight");
+                item.setSize(newWidth, newHeight);
+            }
+        }));
+        
+        
     }
 }
