ff100153ea1c67b9def4ae654f7b9b8547168503
marianbuenosayres <marianbuenosayres@gmail.com>
2011-06-30 11:24:56 -0300

    First corrections to run gwt test cases from maven

--- jbpm-gwt/jbpm-gwt-form-builder/src/test/java/org/jbpm/formbuilder/client/form/FormEncodingClientFactoryGwtTest.java
+++ jbpm-gwt/jbpm-gwt-form-builder/src/test/java/org/jbpm/formbuilder/client/form/FormEncodingClientFactoryGwtTest.java
@@ -8,151 +8,34 @@ import com.google.gwt.junit.client.GWTTestCase;
 
 public class FormEncodingClientFactoryGwtTest extends GWTTestCase {
 
-    private static final String JSON = "{"+
-    "\"name\": \"myForm\","+
-    "\"action\": \"complete\","+
-    "\"taskId\": \"myTask\","+
-    "\"documentation\": \"This is documentation\","+
-    "\"enctype\": \"multipart/form-data\","+
-    "\"lastModified\": \"1309368553625\","+
-    "\"method\": \"POST\","+
-    "\"formItems\": ["+
-    "    {"+
-    "        \"styleClass\": null,"+
-    "        \"value\": \"Login Form Template\","+
-    "        \"typeId\": \"header\","+
-    "        \"input\": null,"+
-    "        \"@className\": \"org.jbpm.formbuilder.shared.rep.items.HeaderRepresentation\","+
-    "        \"width\": null,"+
-    "        \"height\": null,"+
-    "        \"output\": null,"+
-    "        \"cssId\": null,"+
-    "        \"cssName\": null,"+
-    "        \"itemValidations\": []"+
-    "    },"+
-    "    {"+
-    "        \"input\": null,"+
-    "        \"width\": \"100%\","+
-    "        \"rows\": 3,"+
-    "        \"output\": null,"+
-    "        \"itemValidations\": [],"+
-    "        \"columns\": 2,"+
-    "        \"@className\": \"org.jbpm.formbuilder.shared.rep.items.TableRepresentation\","+
-    "        \"typeId\": \"table\","+
-    "        \"cellPadding\": 0,"+
-    "        \"height\": \"200px\","+
-    "        \"borderWidth\": 1,"+
-    "        \"cellSpacing\": 0,"+
-    "        \"elements\": ["+
-    "            ["+
-    "                {"+
-    "                    \"value\": \"Password\","+
-    "                    \"typeId\": \"label\","+
-    "                    \"input\": null,"+
-    "                    \"@className\": \"org.jbpm.formbuilder.shared.rep.items.LabelRepresentation\","+
-    "                    \"width\": null,"+
-    "                    \"height\": null,"+
-    "                    \"output\": null,"+
-    "                    \"cssName\": null,"+
-    "                    \"itemValidations\": [],"+
-    "                    \"id\": null"+
-    "                },"+
-    "                {"+
-    "                    \"maxLength\": null,"+
-    "                    \"typeId\": \"textField\","+
-    "                    \"input\": null,"+
-    "                    \"@className\": \"org.jbpm.formbuilder.shared.rep.items.TextFieldRepresentation\","+
-    "                    \"width\": \"160px\","+
-    "                    \"defaultValue\": null,"+
-    "                    \"height\": \"21px\","+
-    "                    \"output\": null,"+
-    "                    \"itemValidations\": [],"+
-    "                    \"name\": \"usr\","+
-    "                    \"id\": null"+
-    "                }"+
-    "            ],"+
-    "            ["+
-    "                {"+
-    "                    \"value\": null,"+
-    "                    \"typeId\": \"label\","+
-    "                    \"input\": null,"+
-    "                    \"@className\": \"org.jbpm.formbuilder.shared.rep.items.LabelRepresentation\","+
-    "                    \"width\": null,"+
-    "                    \"height\": null,"+
-    "                    \"output\": null,"+
-    "                    \"cssName\": null,"+
-    "                    \"itemValidations\": [],"+
-    "                    \"id\": null"+
-    "                },"+
-    "                {"+
-    "                    \"maxLength\": null,"+
-    "                    \"typeId\": \"textField\","+
-    "                    \"input\": null,"+
-    "                    \"@className\": \"org.jbpm.formbuilder.shared.rep.items.TextFieldRepresentation\","+
-    "                    \"width\": \"160px\","+
-    "                    \"defaultValue\": null,"+
-    "                    \"height\": \"21px\","+
-    "                    \"output\": null,"+
-    "                    \"itemValidations\": [],"+
-    "                    \"name\": \"pwd\","+
-    "                    \"id\": null"+
-    "                }"+
-    "            ],"+
-    "            ["+
-    "                null,"+
-    "                {"+
-    "                    \"onClickScript\": {"+
-    "                        \"type\": \"text/javascript\","+
-    "                        \"documentation\": null,"+
-    "                        \"content\": \"document.forms[0].submit();\","+
-    "                        \"src\": null,"+
-    "                        \"invokeFunction\": null,"+
-    "                        \"id\": null"+
-    "                    },"+
-    "                    \"typeId\": \"completeButton\","+
-    "                    \"input\": null,"+
-    "                    \"@className\": \"org.jbpm.formbuilder.shared.rep.items.CompleteButtonRepresentation\","+
-    "                    \"text\": \"Login\","+
-    "                    \"width\": \"140px\","+
-    "                    \"height\": \"25px\","+
-    "                    \"output\": null,"+
-    "                    \"itemValidations\": [],"+
-    "                    \"name\": null,"+
-    "                    \"id\": null"+
-    "                }"+
-    "            ]"+
-    "        ]"+
-    "    }"+
-    "],"+
-    "\"formValidations\": [],"+
-    "\"inputs\": {"+
-    "    \"in2\": {"+
-    "        \"@className\": \"org.jbpm.formbuilder.shared.rep.InputData\","+
-    "        \"mimeType\": null,"+
-    "        \"value\": \"${process.dataY}\","+
-    "        \"name\": \"in2\","+
-    "        \"formatter\": null"+
-    "    },"+ 
-    "    \"in1\": {"+
-    "        \"@className\": \"org.jbpm.formbuilder.shared.rep.InputData\","+
-    "        \"mimeType\": null,"+
-    "        \"value\": \"${process.dataX}\","+
-    "        \"name\": \"in1\","+
-    "        \"formatter\": null"+
-    "    }"+
-    "},"+
-    "\"outputs\": {"+
-    "    \"process.dataZ\": {"+
-    "        \"@className\": \"org.jbpm.formbuilder.shared.rep.OutputData\","+
-    "        \"mimeType\": null,"+
-    "        \"value\": \"${pwd}\","+
-    "        \"name\": \"process.dataZ\","+
-    "        \"formatter\": null"+
-    "    }"+
-    "},"+
-    "\"onLoadScripts\": [],"+
-    "\"onSubmitScripts\": []"+ 
-    "}";
+    private static final String JSON = "{\"name\": \"myForm\",\"action\": \"complete\","+
+        "\"taskId\": \"myTask\",\"documentation\": \"This is documentation\",\"enctype\": \"multipart/form-data\","+
+        "\"lastModified\": \"1309368553625\",\"method\": \"POST\",\"formItems\": [{\"styleClass\": null,"+
+        "\"value\": \"Login Form Template\",\"typeId\": \"header\",\"input\": null,"+
+        "\"@className\": \"org.jbpm.formbuilder.shared.rep.items.HeaderRepresentation\","+
+        "\"width\": null,\"height\": null,\"output\": null,\"cssId\": null,\"cssName\": null,"+
+        "\"itemValidations\": []},{\"input\": null,\"width\": \"100%\",\"rows\": 3,\"output\": null,"+
+        "\"itemValidations\": [],\"columns\": 2,\"@className\": \"org.jbpm.formbuilder.shared.rep.items.TableRepresentation\","+
+        "\"typeId\": \"table\",\"cellPadding\": 0,\"height\": \"200px\",\"borderWidth\": 1,\"cellSpacing\": 0,\"elements\": ["+
+        "[{\"value\": \"Password\",\"typeId\": \"label\",\"input\": null,\"@className\": " +
+        "\"org.jbpm.formbuilder.shared.rep.items.LabelRepresentation\",\"width\": null,\"height\": null,\"output\": null,\"cssName\": null,"+
+        "\"itemValidations\": [],\"id\": null},{\"maxLength\": null,\"typeId\": \"textField\",\"input\": null,\"@className\": " +
+        "\"org.jbpm.formbuilder.shared.rep.items.TextFieldRepresentation\",\"width\": \"160px\",\"defaultValue\": null,\"height\": \"21px\"," +
+        "\"output\": null,\"itemValidations\": [],\"name\": \"usr\",\"id\": null}],[{\"value\": null,\"typeId\": \"label\",\"input\": null,"+
+        "\"@className\": \"org.jbpm.formbuilder.shared.rep.items.LabelRepresentation\",\"width\": null,\"height\": null,"+
+        "\"output\": null,\"cssName\": null,\"itemValidations\": [],\"id\": null},{\"maxLength\": null,\"typeId\": \"textField\","+
+        "\"input\": null,\"@className\": \"org.jbpm.formbuilder.shared.rep.items.TextFieldRepresentation\",\"width\": \"160px\","+
+        "\"defaultValue\": null,\"height\": \"21px\",\"output\": null,\"itemValidations\": [],\"name\": \"pwd\",\"id\": null"+
+        "}],[null,{\"onClickScript\": {\"type\": \"text/javascript\",\"documentation\": null,\"content\": \"document.forms[0].submit();\","+
+        "\"src\": null,\"invokeFunction\": null,\"id\": null},\"typeId\": \"completeButton\",\"input\": null,"+
+        "\"@className\": \"org.jbpm.formbuilder.shared.rep.items.CompleteButtonRepresentation\",\"text\": \"Login\","+
+        "\"width\": \"140px\",\"height\": \"25px\",\"output\": null,\"itemValidations\": [],\"name\": null,\"id\": null"+
+        "}]]}],\"formValidations\": [],\"inputs\": {\"in2\": {\"@className\": \"org.jbpm.formbuilder.shared.rep.InputData\","+
+        "\"mimeType\": null,\"value\": \"${process.dataY}\",\"name\": \"in2\",\"formatter\": null},\"in1\": {"+
+        "\"@className\": \"org.jbpm.formbuilder.shared.rep.InputData\",\"mimeType\": null,\"value\": \"${process.dataX}\","+
+        "\"name\": \"in1\",\"formatter\": null}},\"outputs\": {\"process.dataZ\": {"+
+        "\"@className\": \"org.jbpm.formbuilder.shared.rep.OutputData\",\"mimeType\": null,\"value\": \"${pwd}\"," +
+        "\"name\": \"process.dataZ\",\"formatter\": null}},\"onLoadScripts\": [],\"onSubmitScripts\": []}";
     
     @Override
     public String getModuleName() {
