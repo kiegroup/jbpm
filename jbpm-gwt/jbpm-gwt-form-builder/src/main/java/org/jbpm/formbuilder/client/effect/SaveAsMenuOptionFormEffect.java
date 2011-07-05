66ad2bda8e99009fb903bf8422c56f0ee045b715
marianbuenosayres <marianbuenosayres@gmail.com>
2011-07-05 12:06:13 -0300

    Added group name option to popup menu "save as menu option"

--- jbpm-gwt/jbpm-gwt-form-builder/src/main/java/org/jbpm/formbuilder/client/effect/SaveAsMenuOptionFormEffect.java
+++ jbpm-gwt/jbpm-gwt-form-builder/src/main/java/org/jbpm/formbuilder/client/effect/SaveAsMenuOptionFormEffect.java
@@ -16,7 +16,7 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.TextBox;
@@ -25,6 +25,7 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 public class SaveAsMenuOptionFormEffect extends FBFormEffect {
 
     private String newMenuOptionName;
+    private String groupName;
     private EventBus bus = FormBuilderGlobals.getInstance().getEventBus();
     
     public SaveAsMenuOptionFormEffect() {
@@ -35,6 +36,10 @@ public class SaveAsMenuOptionFormEffect extends FBFormEffect {
         this.newMenuOptionName = newMenuOptionName;
     }
     
+    public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+    
     @Override
     protected void createStyles() {
         final FBFormItem formItem = super.getItem();
@@ -42,16 +47,19 @@ public class SaveAsMenuOptionFormEffect extends FBFormEffect {
         //TODO fire UndoableEvent
         Map<String, Object> dataSnapshot = new HashMap<String, Object>();
         dataSnapshot.put("menuItem", menuItem);
+        dataSnapshot.put("groupName", groupName);
         bus.fireEvent(new UndoableEvent(dataSnapshot, new UndoableEventHandler() {
             public void onEvent(UndoableEvent event) {  }
             public void undoAction(UndoableEvent event) {
                 FBMenuItem menuItem = (FBMenuItem) event.getData("menuItem");
-                MenuItemRemoveEvent mevent = new MenuItemRemoveEvent(menuItem, "Custom");
+                String groupName = (String) event.getData("groupName");
+                MenuItemRemoveEvent mevent = new MenuItemRemoveEvent(menuItem, groupName);
                 FormBuilderGlobals.getInstance().getEventBus().fireEvent(mevent);
             }
             public void doAction(UndoableEvent event) {
                 FBMenuItem menuItem = (FBMenuItem) event.getData("menuItem");
-                MenuItemAddedEvent mevent = new MenuItemAddedEvent(menuItem, "Custom");
+                String groupName = (String) event.getData("groupName");
+                MenuItemAddedEvent mevent = new MenuItemAddedEvent(menuItem, groupName);
                 FormBuilderGlobals.getInstance().getEventBus().fireEvent(mevent);
             }
         }));
@@ -61,21 +69,26 @@ public class SaveAsMenuOptionFormEffect extends FBFormEffect {
     @Override
     public PopupPanel createPanel() {
         final PopupPanel panel = new PopupPanel();
-        panel.setSize("200px", "60px");
+        panel.setSize("250px", "90px");
         VerticalPanel vPanel = new VerticalPanel();
-        HorizontalPanel hPanel = new HorizontalPanel();
-        hPanel.add(new Label("Option Name:"));
-        final TextBox labelBox = new TextBox();
-        hPanel.add(labelBox);
+        Grid grid = new Grid(2, 2);
+        final TextBox optionNameBox = new TextBox();
+        final TextBox groupNameBox = new TextBox();
+        grid.setWidget(0, 0, new Label("Option Name:"));
+        grid.setWidget(0, 1, optionNameBox);
+        grid.setWidget(1, 0, new Label("Group Name:"));
+        grid.setWidget(1, 1, groupNameBox);
+        
         Button applyButton = new Button("Apply");
         applyButton.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
-                setNewMenuOptionName(labelBox.getValue());
+                setNewMenuOptionName(optionNameBox.getValue());
+                setGroupName(groupNameBox.getValue());
                 createStyles();
                 panel.hide();
             }
         });
-        vPanel.add(hPanel);
+        vPanel.add(grid);
         vPanel.add(applyButton);
         panel.add(vPanel);
         return panel;
