5a69c73e0a06bd53293d96cb9ca506ca101a3a1b
marianbuenosayres <marianbuenosayres@gmail.com>
2011-07-15 18:35:51 -0300

    Added refactor (part 2/2)

--- jbpm-gwt/jbpm-gwt-form-builder/src/main/java/org/jbpm/formbuilder/client/bus/NotificationEventHandler.java
+++
@@ -1,8 +0,0 @@
-package org.jbpm.formbuilder.client.bus;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface NotificationEventHandler extends EventHandler {
-
-    void onEvent(NotificationEvent event);
-}
