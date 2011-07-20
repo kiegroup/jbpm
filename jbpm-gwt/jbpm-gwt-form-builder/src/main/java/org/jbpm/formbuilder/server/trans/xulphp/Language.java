1fb35a9d79859d05e719ec32124b512703f416dd
marianbuenosayres <marianbuenosayres@gmail.com>
2011-07-20 16:35:17 -0300

    Added converter for html tag

--- jbpm-gwt/jbpm-gwt-form-builder/src/main/java/org/jbpm/formbuilder/server/trans/xulphp/Language.java
+++ jbpm-gwt/jbpm-gwt-form-builder/src/main/java/org/jbpm/formbuilder/server/trans/xulphp/Language.java
@@ -1,5 +1,8 @@
 package org.jbpm.formbuilder.server.trans.xulphp;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.jbpm.formbuilder.server.trans.ScriptingLanguage;
 
 public class Language extends ScriptingLanguage {
@@ -11,8 +14,12 @@ public class Language extends ScriptingLanguage {
     }
     
     public String toXulEscapedHtml(String html) {
-        String retval = html.replaceAll("@<(.+?) @i", "<html:$1 xmlns:html=\"http://www.w3.org/1999/xhtml\" ");
-        retval = retval.replaceAll("@</(.+?)>@i", "</html:$1>");
+        Matcher matcher1 = Pattern.compile("<([a-zA-Z\\n\\r]+?) (.+?)>", Pattern.MULTILINE).matcher(html);
+        String retval = matcher1.replaceAll("<html\\:$1 xmlns:html=\"http://www.w3.org/1999/xhtml\" $2>");
+        Matcher matcher2 = Pattern.compile("<([a-zA-Z\\n\\r]+?)(?m)>", Pattern.MULTILINE).matcher(retval);
+        retval = matcher2.replaceAll("<html\\:$1 xmlns:html=\"http://www.w3.org/1999/xhtml\">");
+        Matcher matcher3 = Pattern.compile("</([a-zA-Z\\n\\r]+?)>", Pattern.MULTILINE).matcher(retval);
+        retval = matcher3.replaceAll("</html:$1>");
         return retval;
     }
 }
