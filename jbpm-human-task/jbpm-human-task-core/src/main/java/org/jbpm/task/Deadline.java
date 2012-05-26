d2d9ea888bb9f1d0581e22da32ec0b5d0510f11d
Marco Rietveld <mrietvel@redhat.com>
2012-05-26 04:33:39 -0400

    BZ 823773 - query compatibility with DB2 (cursor + tx problems)

--- jbpm-human-task/jbpm-human-task-core/src/main/java/org/jbpm/task/Deadline.java
+++ jbpm-human-task/jbpm-human-task-core/src/main/java/org/jbpm/task/Deadline.java
@@ -23,7 +23,16 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
 
 import org.jbpm.task.utils.CollectionUtils;
 
