c01b7ec3686ed91b34cb0d410e42ad9ae1d26c53
Sebastian C <calbazasebastian@yahoo.com>
2012-10-19 18:34:55 +0300

    updated test and added new bpmn def

--- jbpm-test/src/test/java/org/jbpm/concurrency/ConcurrentRequestsTest.java
+++ jbpm-test/src/test/java/org/jbpm/concurrency/ConcurrentRequestsTest.java
@@ -1,15 +1,22 @@
 package org.jbpm.concurrency;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.persistence.Persistence;
 import javax.transaction.UserTransaction;
 
 import org.drools.runtime.StatefulKnowledgeSession;
 import org.drools.runtime.process.ProcessInstance;
+import org.drools.runtime.process.WorkItem;
+import org.drools.runtime.process.WorkItemHandler;
+import org.drools.runtime.process.WorkItemManager;
 import org.hibernate.cfg.Environment;
 import org.jbpm.concurrency.util.RunThreadCommands;
 import org.jbpm.process.instance.impl.demo.DoNothingWorkItemHandler;
@@ -23,6 +30,7 @@ import bitronix.tm.resource.jdbc.PoolingDataSource;
 public class ConcurrentRequestsTest extends JbpmJUnitTestCase {
     private Logger logger = LoggerFactory
             .getLogger(ConcurrentRequestsTest.class);
+    final Map<String, Object> results = new HashMap<String, Object>();
 
     public ConcurrentRequestsTest() {
         super(false);
@@ -40,10 +48,10 @@ public class ConcurrentRequestsTest extends JbpmJUnitTestCase {
         ut.begin();
         emf = Persistence.createEntityManagerFactory(
                 "org.jbpm.persistence.jpa", p);
-        
-        final StatefulKnowledgeSession ksession = createKnowledgeSession("humantask.bpmn");
+        final TestWorkItemHandler twh1 = new TestWorkItemHandler();
+        final StatefulKnowledgeSession ksession = createKnowledgeSession("concurrency_humantask.bpmn");
         ksession.getWorkItemManager().registerWorkItemHandler("Human Task",
-                new DoNothingWorkItemHandler());
+                twh1);
         ut.commit();
         mySetupPoolingDataSource("2");
         p = new Properties();
@@ -53,10 +61,11 @@ public class ConcurrentRequestsTest extends JbpmJUnitTestCase {
         ut.begin();
         emf = Persistence.createEntityManagerFactory(
                 "org.jbpm.persistence.jpa", p);
-       
-        final StatefulKnowledgeSession ksession2 = createKnowledgeSession("humantask.bpmn");
+
+        final TestWorkItemHandler twh2 = new TestWorkItemHandler();
+        final StatefulKnowledgeSession ksession2 = createKnowledgeSession("concurrency_humantask.bpmn");
         ksession2.getWorkItemManager().registerWorkItemHandler("Human Task",
-                new DoNothingWorkItemHandler());
+                twh2);
         ut.commit();
         mySetupPoolingDataSource("3");
         ut = (UserTransaction) new InitialContext()
@@ -66,129 +75,59 @@ public class ConcurrentRequestsTest extends JbpmJUnitTestCase {
         p.setProperty(Environment.DATASOURCE, "jdbc/3");
         emf = Persistence.createEntityManagerFactory(
                 "org.jbpm.persistence.jpa", p);
-       
-        final StatefulKnowledgeSession ksession3 = createKnowledgeSession("humantask.bpmn");
+
+        final TestWorkItemHandler twh3 = new TestWorkItemHandler();
+        final StatefulKnowledgeSession ksession3 = createKnowledgeSession("concurrency_humantask.bpmn");
         ksession3.getWorkItemManager().registerWorkItemHandler("Human Task",
-                new DoNothingWorkItemHandler());
+                twh3);
         ut.commit();
+
         RunThreadCommands commands = new RunThreadCommands();
 
         final List<ProcessInstance> pis = new ArrayList<ProcessInstance>();
-        for (int i = 0; i < 150; i++) {
+        for (int i = 0; i < 10; i++) {
             final int ii = i;
             commands.addCommand(new RunThreadCommands.Command() {
                 public void doWork() throws Exception {
-                    UserTransaction ut = (UserTransaction) new InitialContext()
-                            .lookup("java:comp/UserTransaction");
-                    ut.begin();
-                    try {
-                        logger.info("start proces " + ii + " for session 1");
-                        ProcessInstance processInstance = ksession
-                                .startProcess("com.sample.bpmn.hello");
-                         Thread.sleep(100);
-                        ut.commit();
-                        pis.add(processInstance);
-
-                    } catch (Exception e) {
-                        ut.rollback();
-                        throw e;
-                    }
-
-                }
-            });
-            commands.addCommand(new RunThreadCommands.Command() {
-                public void doWork() throws Exception {
-                    UserTransaction ut = (UserTransaction) new InitialContext()
-                            .lookup("java:comp/UserTransaction");
-                    ut.begin();
-                    try {
-                        logger.info("start proces " + ii + " for session 1-b");
-                        ProcessInstance processInstance = ksession
-                                .startProcess("com.sample.bpmn.hello");
-                        Thread.sleep(100);
-                        ut.commit();
-                        pis.add(processInstance);
-
-                    } catch (Exception e) {
-                        ut.rollback();
-                        throw e;
-                    }
-
+                    logger.info("start proces " + ii + " for session 1");
+                    runProcess(ksession, twh1);
                 }
             });
             commands.addCommand(new RunThreadCommands.Command() {
 
                 public void doWork() throws Exception {
-                    UserTransaction ut = (UserTransaction) new InitialContext()
-                            .lookup("java:comp/UserTransaction");
-                    ut.begin();
-                    try {
-                        logger.info("start proces " + ii + " for session 2");
-                        ProcessInstance processInstance = ksession2
-                                .startProcess("com.sample.bpmn.hello");
-                        Thread.sleep(100);
-                        ut.commit();
-                    } catch (Exception e) {
-                        ut.rollback();
-                        throw e;
-                    }
+                    logger.info("start proces " + ii + " for session 1-b");
+                    runProcess(ksession, twh1);
                 }
             });
             commands.addCommand(new RunThreadCommands.Command() {
 
                 public void doWork() throws Exception {
-                    UserTransaction ut = (UserTransaction) new InitialContext()
-                            .lookup("java:comp/UserTransaction");
-                    ut.begin();
+                    logger.info("start proces " + ii + " for session 2");
+                    runProcess(ksession2, twh2);
+                }
+            });
+            commands.addCommand(new RunThreadCommands.Command() {
+
+                public void doWork() throws Exception {
                     logger.info("start proces " + ii + " for session 2-b");
-                    try {
-                        ProcessInstance processInstance = ksession2
-                                .startProcess("com.sample.bpmn.hello");
-                        Thread.sleep(100);
-                        ut.commit();
-                    } catch (Exception e) {
-                        ut.rollback();
-                        throw e;
-                    }
+                    runProcess(ksession2, twh2);
                 }
             });
             commands.addCommand(new RunThreadCommands.Command() {
                 public void doWork() throws Exception {
-                    UserTransaction ut = (UserTransaction) new InitialContext()
-                            .lookup("java:comp/UserTransaction");
-                    ut.begin();
-                    try {
-                        logger.info("start proces " + ii + " for session 3");
-
-                        ProcessInstance processInstance = ksession3
-                                .startProcess("com.sample.bpmn.hello");
-                        Thread.sleep(100);
-                        ut.commit();
-                    } catch (Exception e) {
-                        ut.rollback();
-                        throw e;
-                    }
+                    logger.info("start proces " + ii + " for session 3");
+                    runProcess(ksession3, twh3);
                 }
             });
             commands.addCommand(new RunThreadCommands.Command() {
                 public void doWork() throws Exception {
-                    UserTransaction ut = (UserTransaction) new InitialContext()
-                            .lookup("java:comp/UserTransaction");
-                    ut.begin();
-                    try {
-                        logger.info("start proces " + ii + " for session 3-b");
-                        ProcessInstance processInstance = ksession3
-                                .startProcess("com.sample.bpmn.hello");
-                        Thread.sleep(100);
-                        ut.commit();
-                    } catch (Exception e) {
-                        ut.rollback();
-                        throw e;
-                    }
+                    logger.info("start proces " + ii + " for session 3-b");
+                    runProcess(ksession3, twh3);
                 }
             });
-//
         }
+
         commands.runCommands();
         for (ProcessInstance pi : pis) {
             assertNotNull(ksession.getProcessInstance(pi.getId()));
@@ -213,5 +152,57 @@ public class ConcurrentRequestsTest extends JbpmJUnitTestCase {
         pds.setAllowLocalTransactions(false);
         pds.init();
         return pds;
+    }
+
+    private void runProcess(StatefulKnowledgeSession ksession,
+            TestWorkItemHandler twh) throws Exception {
+
+        UserTransaction ut = (UserTransaction) new InitialContext()
+                .lookup("java:comp/UserTransaction");
+
+        try {
+            ut.begin();
+            ProcessInstance processInstance = ksession
+                    .startProcess("concurrency");
+            Thread.sleep(100);
+            ut.commit();
+        } catch (Exception e) {
+            ut.rollback();
+            throw e;
+        }
+        Thread.sleep(3100);
+        ksession.getWorkItemManager().completeWorkItem(
+                twh.getWorkItem().getId(), results);
+
+        Thread.sleep(3100);
+        ksession.getWorkItemManager().completeWorkItem(
+                twh.getWorkItem().getId(), results);
+
+    }
+
+    public static class TestWorkItemHandler implements WorkItemHandler {
+
+        private List<WorkItem> workItems =Collections.synchronizedList(new ArrayList<WorkItem>());
+
+        public synchronized void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
+            workItems.add(workItem);
+        }
+
+        public void abortWorkItem(WorkItem workItem, WorkItemManager manager) {
+        }
+
+        public synchronized WorkItem getWorkItem() {
+            if (workItems.size() == 0) {
+                return null;
+            }
+            return workItems.remove(0);
+        }
+
+        public List<WorkItem> getWorkItems() {
+            List<WorkItem> result = new ArrayList<WorkItem>(workItems);
+            workItems.clear();
+            return result;
+        }
+
     }
 }
