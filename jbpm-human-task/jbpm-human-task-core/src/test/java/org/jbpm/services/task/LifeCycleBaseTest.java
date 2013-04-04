160264e978d794b5fb088c2fbb47d2fe4f7d284d
alegger <alexander.egger@gmail.com>
2013-04-04 17:35:36 +0200

    Removed double check. Does not add value. OpenJpa does not allow the modification of results.

--- jbpm-human-task/jbpm-human-task-core/src/test/java/org/jbpm/services/task/LifeCycleBaseTest.java
+++ jbpm-human-task/jbpm-human-task-core/src/test/java/org/jbpm/services/task/LifeCycleBaseTest.java
@@ -388,8 +388,6 @@ public abstract class LifeCycleBaseTest extends HumanTaskServicesBaseTest {
         assertEquals(1, allTasks.size());
         List<TaskSummary> personalTasks = taskService.getTasksOwnedByStatus("salaboy", statuses, "en-UK");
         assertEquals(2, personalTasks.size());
-        allTasks.addAll(personalTasks);
-        assertEquals(3, allTasks.size());
 
         Task task1 = taskService.getTaskById(taskId);
         assertEquals(Status.Reserved, task1.getTaskData().getStatus());
@@ -403,8 +401,6 @@ public abstract class LifeCycleBaseTest extends HumanTaskServicesBaseTest {
         assertEquals(2, allTasks.size());
         personalTasks = taskService.getTasksOwnedByStatus("salaboy", statuses, "en-UK");
         assertEquals(1, personalTasks.size());
-        allTasks.addAll(personalTasks);
-        assertEquals(3, allTasks.size());
         
         Task task2 = taskService.getTaskById(taskId);
         assertEquals(Status.Ready, task2.getTaskData().getStatus());
@@ -432,8 +428,6 @@ public abstract class LifeCycleBaseTest extends HumanTaskServicesBaseTest {
         assertEquals(1, allTasks.size());
         personalTasks = taskService.getTasksOwnedByStatus("salaboy", statuses, "en-UK");
         assertEquals(2, personalTasks.size());
-        allTasks.addAll(personalTasks);
-        assertEquals(3, allTasks.size());
         
         
     }
