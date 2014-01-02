d65c7dc185f33c75a175851dc3dbcaaa091040de
Maciej Swiderski <mswiders@redhat.com>
2014-01-02 18:36:28 +0100

    BZ-1040198 - Memory leak in a process with timer when run in Java API by using PerProcessInstance session strategy with disabled persistence due to: java.lang.OutOfMemoryError: unable to create new native thread

--- jbpm-runtime-manager/src/main/java/org/jbpm/runtime/manager/impl/PerProcessInstanceRuntimeManager.java
+++ jbpm-runtime-manager/src/main/java/org/jbpm/runtime/manager/impl/PerProcessInstanceRuntimeManager.java
@@ -27,6 +27,7 @@ import org.drools.persistence.OrderedTransactionSynchronization;
 import org.drools.persistence.TransactionManagerHelper;
 import org.drools.persistence.jta.JtaTransactionManager;
 import org.jbpm.runtime.manager.impl.factory.CDITaskServiceFactory;
+import org.jbpm.runtime.manager.impl.mapper.InMemoryMapper;
 import org.jbpm.runtime.manager.impl.tx.DestroySessionTransactionSynchronization;
 import org.jbpm.runtime.manager.impl.tx.DisposeSessionTransactionSynchronization;
 import org.kie.api.event.process.DefaultProcessEventListener;
@@ -145,10 +146,15 @@ public class PerProcessInstanceRuntimeManager extends AbstractRuntimeManager {
     	if (isClosed()) {
     		throw new IllegalStateException("Runtime manager " + identifier + " is already closed");
     	}
-        removeLocalRuntime(runtime);
-        if (runtime instanceof Disposable && environment.usePersistence()) {
+    	removeLocalRuntime(runtime);
+    	if (runtime instanceof Disposable) {
+        	// special handling for in memory to not allow to dispose if there is any context in the mapper
+        	if (mapper instanceof InMemoryMapper && ((InMemoryMapper)mapper).hasContext(runtime.getKieSession().getId())){
+        		return;
+        	}
             ((Disposable) runtime).dispose();
         }
+        
     }
 
     @Override
@@ -182,7 +188,7 @@ public class PerProcessInstanceRuntimeManager extends AbstractRuntimeManager {
         private RuntimeEngine runtime;
         
         MaintainMappingListener(Integer ksessionId, RuntimeEngine runtime) {
-            this.ksessionId = ksessionId;
+        	this.ksessionId = ksessionId;
             this.runtime = runtime;
         }
         @Override
@@ -191,12 +197,12 @@ public class PerProcessInstanceRuntimeManager extends AbstractRuntimeManager {
             removeLocalRuntime(runtime);
             
             registerDisposeCallback(runtime, 
-                        new DestroySessionTransactionSynchronization(runtime.getKieSession()));            
+                        new DestroySessionTransactionSynchronization(runtime.getKieSession()));   
         }
 
         @Override
         public void beforeProcessStarted(ProcessStartedEvent event) {
-            mapper.saveMapping(ProcessInstanceIdContext.get(event.getProcessInstance().getId()), ksessionId);  
+            mapper.saveMapping(ProcessInstanceIdContext.get(event.getProcessInstance().getId()), ksessionId); 
             saveLocalRuntime(event.getProcessInstance().getId(), runtime);
         }
         
