5c07d523aa186fa1012696a993be7e2f0766c904
Marco Rietveld <mrietvel@redhat.com>
2014-02-10 15:39:22 +0100

    BZ-1058604 - Implement REST operation for cleaning all data .. (cherry-pick from master)

--- jbpm-human-task/jbpm-human-task-audit/src/test/java/org/jbpm/services/task/audit/test/LifeCycleBaseTest.java
+++ jbpm-human-task/jbpm-human-task-audit/src/test/java/org/jbpm/services/task/audit/test/LifeCycleBaseTest.java
@@ -16,14 +16,19 @@
 package org.jbpm.services.task.audit.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.StringReader;
 import java.util.HashMap;
 import java.util.List;
 
-import org.jbpm.services.task.audit.impl.model.GroupAuditTask;
-import org.jbpm.services.task.audit.impl.model.UserAuditTask;
+import org.jbpm.services.task.HumanTaskServicesBaseTest;
+import org.jbpm.services.task.audit.DeleteAuditEventsCommand;
+import org.jbpm.services.task.audit.GetAuditEventsCommand;
 import org.jbpm.services.task.impl.factories.TaskFactory;
+import org.jbpm.services.task.impl.model.BAMTaskSummaryImpl;
+import org.jbpm.services.task.impl.model.command.DeleteBAMTaskSummariesCommand;
+import org.jbpm.services.task.impl.model.command.GetBAMTaskSummariesCommand;
 import org.junit.Test;
 import org.kie.api.task.model.Status;
 import org.kie.api.task.model.Task;
@@ -33,16 +38,13 @@ public abstract class LifeCycleBaseTest extends HumanTaskServicesBaseTest {
 
     @Test
     public void testComplete() {
-        
-
-        // A group as potential owners, should go straight to state Ready to be claimed
+        // One potential owner, should go straight to state Reserved
         String str = "(with (new Task()) { priority = 55, taskData = (with( new TaskData()) { } ), ";
         str += "peopleAssignments = (with ( new PeopleAssignments() ) { potentialOwners = [new Group('Knights Templer' )],businessAdministrators = [ new User('Administrator') ], }),";
         str += "names = [ new I18NText( 'en-UK', 'This is my task name')] })";
 
         Task task = (Task) TaskFactory.evalTask(new StringReader(str));
         taskService.addTask(task, new HashMap<String, Object>());
-
         long taskId = task.getId();
         
          
@@ -69,83 +71,57 @@ public abstract class LifeCycleBaseTest extends HumanTaskServicesBaseTest {
         assertEquals(0, allGroupAuditTasks.size());
         
         // Go straight from Ready to Inprogress
-
         taskService.start(taskId, "Darth Vader");
-
 
         Task task1 = taskService.getTaskById(taskId);
         assertEquals(Status.InProgress, task1.getTaskData().getStatus());
         assertEquals("Darth Vader", task1.getTaskData().getActualOwner().getId());
 
         // Check is Complete
-
         taskService.complete(taskId, "Darth Vader", null);
-
 
         Task task2 = taskService.getTaskById(taskId);
         assertEquals(Status.Completed, task2.getTaskData().getStatus());
         assertEquals("Darth Vader", task2.getTaskData().getActualOwner().getId());
 
-        List<TaskEvent> allTaskEvents = taskAuditService.getAllTaskEvents(taskId);
-               
+        List<TaskEvent> allTaskEvents = taskService.execute(new GetAuditEventsCommand(taskId));
         assertEquals(6, allTaskEvents.size());
-       
-         List<UserAuditTask> allUserAuditTasks = taskAuditService.getAllUserAuditTasks("Darth Vader");
-        
-        assertEquals(1, allUserAuditTasks.size());
-        
-        assertEquals("Completed", allUserAuditTasks.get(0).getStatus());
-       
-        allGroupAuditTasks = taskAuditService.getAllGroupAuditTasks("Knights Templer");
-        
-        assertEquals(0, allGroupAuditTasks.size());
-    }
-    
-    
-    @Test
-    public void testCompleteAssignedTask() {
-        
-
-        // One potential owner, should go straight to state Reserved
-        String str = "(with (new Task()) { priority = 55, taskData = (with( new TaskData()) { } ), ";
-        str += "peopleAssignments = (with ( new PeopleAssignments() ) { potentialOwners = [new User('Darth Vader' )],businessAdministrators = [ new User('Administrator') ], }),";
-        str += "names = [ new I18NText( 'en-UK', 'This is my task name')] })";
-
-
-        Task task = (Task) TaskFactory.evalTask(new StringReader(str));
+     
+        // test DeleteAuditEventsCommand        
+        int numFirstTaskEvents = allTaskEvents.size();
+        task = (Task) TaskFactory.evalTask(new StringReader(str));
         taskService.addTask(task, new HashMap<String, Object>());
-
-        long taskId = task.getId();
-
+        long secondTaskId = task.getId();
+        taskService.claim(secondTaskId, "Darth Vader");    
+        taskService.start(secondTaskId, "Darth Vader");
+        taskService.complete(secondTaskId, "Darth Vader", null);
+       
+        allTaskEvents = taskService.execute(new GetAuditEventsCommand());
+        int numTaskEvents = allTaskEvents.size();
+        assertTrue("Expected more than " + numFirstTaskEvents + " events: "+ numTaskEvents, numTaskEvents > numFirstTaskEvents);
         
+        taskService.execute(new DeleteAuditEventsCommand(taskId));
+        allTaskEvents = taskService.execute(new GetAuditEventsCommand());
+        assertEquals(numTaskEvents - numFirstTaskEvents, allTaskEvents.size());
         
-        // Go straight from Ready to Inprogress
-
-        taskService.start(taskId, "Darth Vader");
-
-
-        Task task1 = taskService.getTaskById(taskId);
-        assertEquals(Status.InProgress, task1.getTaskData().getStatus());
-        assertEquals("Darth Vader", task1.getTaskData().getActualOwner().getId());
-
-        // Check is Complete
-
-        taskService.complete(taskId, "Darth Vader", null);
-
-
-        Task task2 = taskService.getTaskById(taskId);
-        assertEquals(Status.Completed, task2.getTaskData().getStatus());
-        assertEquals("Darth Vader", task2.getTaskData().getActualOwner().getId());
-
-        List<TaskEvent> allTaskEvents = taskAuditService.getAllTaskEvents(taskId);
-               
-        assertEquals(3, allTaskEvents.size());
-        List<UserAuditTask> allUserAuditTasks = taskAuditService.getAllUserAuditTasks("Darth Vader");
+        taskService.execute(new DeleteAuditEventsCommand());
+        allTaskEvents = taskService.execute(new GetAuditEventsCommand());
+        assertEquals(0, allTaskEvents.size());
         
-        assertEquals(1, allUserAuditTasks.size());
+        // test get/delete BAM Task summaries commands
+        List<BAMTaskSummaryImpl> bamTaskList = taskService.execute(new GetBAMTaskSummariesCommand());
+        assertEquals( "BAM Task Summary list size: ", 2, bamTaskList.size());
         
-        assertEquals("Completed", allUserAuditTasks.get(0).getStatus());
+        taskService.execute(new DeleteBAMTaskSummariesCommand(taskId));
+        bamTaskList = taskService.execute(new GetBAMTaskSummariesCommand());
+        assertEquals( "BAM Task Summary list size after delete (task id: " + taskId + ") : ", 1, bamTaskList.size());
         
+        bamTaskList = taskService.execute(new GetBAMTaskSummariesCommand(secondTaskId));
+        assertEquals( "BAM Task Summary list size after delete (task id: " + taskId + ") : ", 1, bamTaskList.size());
+        
+        taskService.execute(new DeleteBAMTaskSummariesCommand());
+        bamTaskList = taskService.execute(new GetBAMTaskSummariesCommand());
+        assertEquals( "BAM Task Summary list size after delete (task id: " + taskId + ") : ", 0, bamTaskList.size());
     }
     
     
