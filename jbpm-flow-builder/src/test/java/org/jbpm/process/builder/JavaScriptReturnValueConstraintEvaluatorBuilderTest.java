62b48ce812ec6af0adab53a124c767430e9ba6c0
domhanak <domin.hanak@gmail.com>
2015-07-03 08:30:11 +0200

    BPMSPL-128 - jBPM Core - Correct added tests and improved existing test for ScriptTask with JavaScript

--- jbpm-flow-builder/src/test/java/org/jbpm/process/builder/JavaScriptReturnValueConstraintEvaluatorBuilderTest.java
+++ jbpm-flow-builder/src/test/java/org/jbpm/process/builder/JavaScriptReturnValueConstraintEvaluatorBuilderTest.java
@@ -1,7 +1,105 @@
 package org.jbpm.process.builder;
 
-/**
- * Created by dhanak on 7/1/15.
- */
+
+import org.drools.compiler.builder.impl.KnowledgeBuilderImpl;
+import org.drools.compiler.compiler.DialectCompiletimeRegistry;
+import org.drools.compiler.compiler.ReturnValueDescr;
+import org.drools.compiler.lang.descr.ProcessDescr;
+import org.drools.core.common.InternalKnowledgeRuntime;
+import org.drools.core.definitions.InternalKnowledgePackage;
+import org.drools.core.definitions.impl.KnowledgePackageImpl;
+import org.jbpm.process.builder.dialect.javascript.JavaScriptProcessDialect;
+import org.jbpm.process.builder.dialect.javascript.JavaScriptReturnValueEvaluatorBuilder;
+import org.jbpm.process.instance.impl.ReturnValueConstraintEvaluator;
+import org.jbpm.ruleflow.instance.RuleFlowProcessInstance;
+import org.jbpm.workflow.core.Constraint;
+import org.jbpm.workflow.core.impl.ConstraintImpl;
+import org.jbpm.workflow.core.impl.WorkflowProcessImpl;
+import org.jbpm.workflow.instance.node.SplitInstance;
+import org.junit.Test;
+import org.kie.internal.KnowledgeBase;
+import org.kie.internal.KnowledgeBaseFactory;
+import org.kie.internal.definition.KnowledgePackage;
+import org.kie.internal.runtime.StatefulKnowledgeSession;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
 public class JavaScriptReturnValueConstraintEvaluatorBuilderTest {
+    @Test
+    public void testSimpleReturnValueConstraintEvaluator() throws Exception {
+        final InternalKnowledgePackage pkg = new KnowledgePackageImpl( "pkg1" );
+
+        ProcessDescr processDescr = new ProcessDescr();
+        processDescr.setClassName( "Process1" );
+        processDescr.setName( "Process1" );
+
+        WorkflowProcessImpl process = new WorkflowProcessImpl();
+        process.setName( "Process1" );
+        process.setPackageName("pkg1");
+
+        ReturnValueDescr descr = new ReturnValueDescr();
+        descr.setText("function validate() {return true;} validate();");
+
+        KnowledgeBuilderImpl pkgBuilder = new KnowledgeBuilderImpl( pkg );
+        DialectCompiletimeRegistry dialectRegistry = pkgBuilder.getPackageRegistry( pkg.getName() ).getDialectCompiletimeRegistry();
+
+        ProcessBuildContext context = new ProcessBuildContext( pkgBuilder,
+                pkg,
+                process,
+                processDescr,
+                dialectRegistry,
+                null );
+
+        pkgBuilder.addPackageFromDrl(new StringReader("package pkg1;\n global Boolean value;\n global Integer count;"));
+
+        ReturnValueConstraintEvaluator node = new ReturnValueConstraintEvaluator();
+
+        final JavaScriptReturnValueEvaluatorBuilder builder = new JavaScriptReturnValueEvaluatorBuilder();
+        builder.build(context,
+                node,
+                descr,
+                null);
+
+        final KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
+        List<KnowledgePackage> packages = new ArrayList<KnowledgePackage>();
+        packages.add( pkgBuilder.getPackage() );
+        kbase.addKnowledgePackages(packages);
+        final StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
+
+
+        RuleFlowProcessInstance processInstance = new RuleFlowProcessInstance();
+        processInstance.setKnowledgeRuntime((InternalKnowledgeRuntime) ksession);
+
+        SplitInstance splitInstance = new SplitInstance();
+        splitInstance.setProcessInstance(processInstance);
+
+        ksession.setGlobal("value", false);
+        ksession.setGlobal("count", 101);
+
+
+        Constraint constraint = new ConstraintImpl();
+        constraint.setDialect("JavaScript");
+        constraint.setConstraint("value === false && count > 100");
+        node.setConstraint(constraint.getConstraint());
+
+        assertTrue(node.evaluate(splitInstance,
+                        null,
+                        constraint)
+        );
+
+        // Set global values to break constraints
+        ksession.setGlobal("value", true);
+        ksession.setGlobal("count", 99);
+
+        // Returns true because constraint argument is ignored -> intended?
+        assertTrue(node.evaluate(splitInstance,
+                        null,
+                        constraint)
+        );
+    }
+
 }
