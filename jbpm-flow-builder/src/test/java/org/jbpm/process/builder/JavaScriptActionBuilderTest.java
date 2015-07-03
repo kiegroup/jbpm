c60cff0a631591dff77154f8f50ebad855162f49
domhanak <domin.hanak@gmail.com>
2015-07-03 08:25:37 +0200

    BPMSPL-128 - jBPM Core - Add tests to cover JavaScript functionality

---
+++ jbpm-flow-builder/src/test/java/org/jbpm/process/builder/JavaScriptActionBuilderTest.java
@@ -0,0 +1,7 @@
+package org.jbpm.process.builder;
+
+/**
+ * Created by dhanak on 6/30/15.
+ */
+public class JavaScriptActionBuilderTest {
+}
---
+++ jbpm-flow-builder/src/test/java/org/jbpm/process/builder/JavaScriptReturnValueConstraintEvaluatorBuilderTest.java
@@ -0,0 +1,7 @@
+package org.jbpm.process.builder;
+
+/**
+ * Created by dhanak on 7/1/15.
+ */
+public class JavaScriptReturnValueConstraintEvaluatorBuilderTest {
+}
