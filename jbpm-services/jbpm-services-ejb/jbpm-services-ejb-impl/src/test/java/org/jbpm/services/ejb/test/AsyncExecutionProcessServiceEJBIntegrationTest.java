3f194bcdd6d61057d472e91d8f466c1819b96524
Petr Siroky <psiroky@redhat.com>
2016-02-12 10:01:48 +0100

    fix after change in Drools: install testing kjars instead of deploying

--- jbpm-services/jbpm-services-ejb/jbpm-services-ejb-impl/src/test/java/org/jbpm/services/ejb/test/AsyncExecutionProcessServiceEJBIntegrationTest.java
+++ jbpm-services/jbpm-services-ejb/jbpm-services-ejb-impl/src/test/java/org/jbpm/services/ejb/test/AsyncExecutionProcessServiceEJBIntegrationTest.java
@@ -88,7 +88,7 @@ public class AsyncExecutionProcessServiceEJBIntegrationTest extends AbstractTest
             
         }
         MavenRepository repository = getMavenRepository();
-        repository.deployArtifact(releaseId, kJar1, pom);
+        repository.installArtifact(releaseId, kJar1, pom);
 	}
 	
 	private List<DeploymentUnit> units = new ArrayList<DeploymentUnit>();
