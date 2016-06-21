565c8177aad8a7d055cada2e352e50360b6a9989
Marián Macik <mmacik@redhat.com>
2016-06-21 16:47:57 +0200

    Fix a typo in a documentation

--- jbpm-persistence-jpa/src/main/java/org/jbpm/persistence/processinstance/JPAProcessInstanceWorkItemManager.java
+++ jbpm-persistence-jpa/src/main/java/org/jbpm/persistence/processinstance/JPAProcessInstanceWorkItemManager.java
@@ -6,7 +6,7 @@ import org.drools.persistence.jpa.processinstance.JPAWorkItemManager;
 /**
  * This class is a jBPM-specific work item manager that should be used whenever
  * 1. jBPM is in the classpath
- * 2. and persistence is *not* being used.
+ * 2. and persistence is being used.
  */
 public class JPAProcessInstanceWorkItemManager extends JPAWorkItemManager {
 
