2e7b1cc274ec8827881ced7f4fbec1dc254ce550
Tibor Zimanyi <tzimanyi@redhat.com>
2016-10-14 18:01:06 +0200

    JBPM-5369 - fix for RuleFlowProcessValidator concurrency issue - removed duplicite validation of start and end nodes.

--- jbpm-flow/src/main/java/org/jbpm/ruleflow/core/RuleFlowProcess.java
+++ jbpm-flow/src/main/java/org/jbpm/ruleflow/core/RuleFlowProcess.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2010 Red Hat, Inc. and/or its affiliates.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,10 +16,8 @@
 
 package org.jbpm.ruleflow.core;
 
-
 import java.util.ArrayList;
 import java.util.List;
-
 import org.jbpm.process.core.context.exception.CompensationScope;
 import org.jbpm.process.core.context.exception.ExceptionScope;
 import org.jbpm.process.core.context.swimlane.SwimlaneContext;
@@ -28,7 +26,9 @@ import org.jbpm.process.core.event.EventFilter;
 import org.jbpm.workflow.core.impl.NodeContainerImpl;
 import org.jbpm.workflow.core.impl.WorkflowProcessImpl;
 import org.jbpm.workflow.core.node.ConstraintTrigger;
+import org.jbpm.workflow.core.node.EndNode;
 import org.jbpm.workflow.core.node.EventTrigger;
+import org.jbpm.workflow.core.node.FaultNode;
 import org.jbpm.workflow.core.node.StartNode;
 import org.jbpm.workflow.core.node.Trigger;
 import org.kie.api.definition.process.Node;
@@ -39,7 +39,7 @@ public class RuleFlowProcess extends WorkflowProcessImpl {
     public static final String RULEFLOW_TYPE = "RuleFlow";
 
     private static final long serialVersionUID = 510l;
-    
+
     public RuleFlowProcess() {
         setType(RULEFLOW_TYPE);
         // TODO create contexts on request ?
@@ -53,11 +53,11 @@ public class RuleFlowProcess extends WorkflowProcessImpl {
         addContext(exceptionScope);
         setDefaultContext(exceptionScope);
     }
-    
+
     public VariableScope getVariableScope() {
         return (VariableScope) getDefaultContext(VariableScope.VARIABLE_SCOPE);
     }
-    
+
     public SwimlaneContext getSwimlaneContext() {
         return (SwimlaneContext) getDefaultContext(SwimlaneContext.SWIMLANE_SCOPE);
     }
@@ -65,7 +65,7 @@ public class RuleFlowProcess extends WorkflowProcessImpl {
     public ExceptionScope getExceptionScope() {
         return (ExceptionScope) getDefaultContext(ExceptionScope.EXCEPTION_SCOPE);
     }
-    
+
     public CompensationScope getCompensationScope() {
         return (CompensationScope) getDefaultContext(CompensationScope.COMPENSATION_SCOPE);
     }
@@ -73,59 +73,74 @@ public class RuleFlowProcess extends WorkflowProcessImpl {
     protected NodeContainer createNodeContainer() {
         return new WorkflowProcessNodeContainer();
     }
-    
+
     public List<Node> getStartNodes() {
-    	return getStartNodes(this.getNodes());
+        return getStartNodes(this.getNodes());
     }
-    
+
     public static List<Node> getStartNodes(Node[] nodes) {
         List<Node> startNodes = new ArrayList<Node>();
-        for (int i = 0; i < nodes.length; i++) {
-            if (nodes[i] instanceof StartNode) {
-            	startNodes.add((StartNode) nodes[i]);
+        for (Node node : nodes) {
+            if (node instanceof StartNode) {
+                startNodes.add(node);
             }
         }
-        
+
         return startNodes;
     }
-    
+
+    public List<Node> getEndNodes() {
+        return getEndNodes(this.getNodes());
+    }
+
+    public static List<Node> getEndNodes(Node[] nodes) {
+        final List<Node> endNodes = new ArrayList<Node>();
+        for (Node node : nodes) {
+            if (node instanceof EndNode || node instanceof FaultNode) {
+                endNodes.add(node);
+            }
+        }
+
+        return endNodes;
+    }
+
     public StartNode getStart(String trigger) {
         Node[] nodes = getNodes();
-        
+
         for (int i = 0; i < nodes.length; i++) {
             if (nodes[i] instanceof StartNode) {
-            	
-            	StartNode start = ((StartNode) nodes[i]);
+
+                StartNode start = ((StartNode) nodes[i]);
                 // return start node that is not event based node
-                if (trigger == null && ((start.getTriggers() == null 
+                if (trigger == null && ((start.getTriggers() == null
                         || start.getTriggers().isEmpty())
                         && start.getTimer() == null)) {
                     return start;
                 } else {
-                	if (start.getTriggers() != null) {
-	                	for (Trigger t : start.getTriggers()) {
-	                		if (t instanceof EventTrigger) {
-	                			for ( EventFilter filter : ((EventTrigger) t).getEventFilters() ) {
-						            if ( filter.acceptsEvent( trigger, null ) ) {
-						                return start;
-						            }
-						        }
-	                		} else if (t instanceof ConstraintTrigger && "conditional".equals(trigger)) {
-	                			return start;
-	                		}
-	                	}
-                	} else if (start.getTimer() != null) {
-                	
-                		if ("timer".equals(trigger)) {
-                			return start;
-                		}
-                	}
+                    if (start.getTriggers() != null) {
+                        for (Trigger t : start.getTriggers()) {
+                            if (t instanceof EventTrigger) {
+                                for (EventFilter filter : ((EventTrigger) t).getEventFilters()) {
+                                    if (filter.acceptsEvent(trigger, null)) {
+                                        return start;
+                                    }
+                                }
+                            } else if (t instanceof ConstraintTrigger && "conditional".equals(trigger)) {
+                                return start;
+                            }
+                        }
+                    } else if (start.getTimer() != null) {
+
+                        if ("timer".equals(trigger)) {
+                            return start;
+                        }
+                    }
                 }
             }
         }
         return null;
     }
-    
+
     public List<StartNode> getTimerStart() {
         Node[] nodes = getNodes();
 
@@ -143,7 +158,7 @@ public class RuleFlowProcess extends WorkflowProcessImpl {
     }
 
     private class WorkflowProcessNodeContainer extends NodeContainerImpl {
-        
+
         private static final long serialVersionUID = 510l;
 
         protected void validateAddNode(Node node) {
@@ -153,11 +168,11 @@ public class RuleFlowProcess extends WorkflowProcessImpl {
                 // ignore start nodes that are event based
                 if ((((StartNode) node).getTriggers() == null || ((StartNode) node).getTriggers().isEmpty()) && ((StartNode) node).getTimer() == null) {
                     throw new IllegalArgumentException(
-                        "A RuleFlowProcess cannot have more than one start node!");
+                            "A RuleFlowProcess cannot have more than one start node!");
                 }
             }
         }
-        
+
     }
 
 }
