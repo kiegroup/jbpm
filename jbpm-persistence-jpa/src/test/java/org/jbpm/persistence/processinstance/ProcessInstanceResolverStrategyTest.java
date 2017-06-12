aeb9f3d81ca7b1e5e2dd54c5e2d14bbb6db1381b
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2017-06-12 20:23:58 +0200

    [RHBRMS-2838]:Revert "RHBPMS-4659 - persisting org.slf4j.Logger fails - temp fix for test (#860)"
    This reverts commit 7b812ed07e5252e41ea75eb4e541edd07fb14864.

--- jbpm-persistence-jpa/src/test/java/org/jbpm/persistence/processinstance/ProcessInstanceResolverStrategyTest.java
+++ jbpm-persistence-jpa/src/test/java/org/jbpm/persistence/processinstance/ProcessInstanceResolverStrategyTest.java
@@ -125,7 +125,8 @@ public class ProcessInstanceResolverStrategyTest extends AbstractBaseTest {
         NonSerializableClass processVar = new NonSerializableClass();
         processVar.setString("1234567890");
         params.put(VAR_NAME, processVar);
-        
+        params.put("logger", logger);
+
         // Persist variable
         UserTransaction ut = (UserTransaction) new InitialContext().lookup("java:comp/UserTransaction");
         ut.begin();
