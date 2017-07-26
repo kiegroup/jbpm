512ab23c701dc7999c749e7505bbc18a56bf0aea
Mario Fusco <mario.fusco@gmail.com>
2017-07-26 19:55:16 +0200

    [DROOLS-1649] ServiceRegistry refactor

--- jbpm-flow-builder/src/main/java/org/jbpm/osgi/flow/compiler/Activator.java
+++ jbpm-flow-builder/src/main/java/org/jbpm/osgi/flow/compiler/Activator.java
@@ -15,33 +15,11 @@
 
 package org.jbpm.osgi.flow.compiler;
 
-import org.drools.compiler.compiler.ProcessBuilderFactory;
-import org.drools.compiler.compiler.ProcessBuilderFactoryService;
-import org.jbpm.process.builder.ProcessBuilderFactoryServiceImpl;
-import org.kie.api.Service;
-import org.kie.api.runtime.manager.RuntimeManagerFactory;
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
+import org.drools.core.osgi.BaseActivator;
 
-import java.util.Hashtable;
+public class Activator extends BaseActivator {
 
-public class Activator
-    implements
-    BundleActivator {
-
-    private ServiceRegistration processBuilderReg;
-
-    public void start(BundleContext bc) throws Exception {
-        this.processBuilderReg = bc.registerService( new String[]{ ProcessBuilderFactoryService.class.getName(), Service.class.getName()},
-                                                                   new ProcessBuilderFactoryServiceImpl(),
-                                                                   new Hashtable() );
-        RuntimeManagerFactory.Factory.reset();
-        ProcessBuilderFactory.reInitializeProvider();        
+    public Activator() {
+        super( Activator.class.getClassLoader() );
     }
-
-    public void stop(BundleContext bc) throws Exception {
-        this.processBuilderReg.unregister();
-    }
-
 }
