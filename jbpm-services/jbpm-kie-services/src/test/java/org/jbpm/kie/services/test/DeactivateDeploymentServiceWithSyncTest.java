43d6e7e8eab0a76a2ae1e149eca51510be6decd7
Marián Macik <macik.marian@gmail.com>
2017-08-14 12:58:54 +0200

    Stabilize DeactivateDeploymentServiceWithSyncTest (#942)

--- jbpm-services/jbpm-kie-services/src/test/java/org/jbpm/kie/services/test/DeactivateDeploymentServiceWithSyncTest.java
+++ jbpm-services/jbpm-kie-services/src/test/java/org/jbpm/kie/services/test/DeactivateDeploymentServiceWithSyncTest.java
@@ -173,31 +173,17 @@ public class DeactivateDeploymentServiceWithSyncTest extends AbstractKieServices
         
         final AtomicBoolean deploymentActive = new AtomicBoolean(true);
         
-        CoundDownDeploymentListener countDownListener = new CoundDownDeploymentListener(1);
-        countDownListener.setDeploy(true);
-        ((ListenerSupport)deploymentService).addListener(countDownListener);
-        ((ListenerSupport)deploymentService).addListener(new DeploymentEventListener() {
-            
-            
-            @Override
-            public void onUnDeploy(DeploymentEvent event) {            
-            }
-            
+        CoundDownDeploymentListener countDownListener = new CoundDownDeploymentListener(1) {
             @Override
             public void onDeploy(DeploymentEvent event) {
+                // This used to use a specific listener for setting the active state
                 deploymentActive.set(event.getDeployedUnit().isActive());
-                
+                super.onDeploy(event);
             }
-            
-            @Override
-            public void onDeactivate(DeploymentEvent event) {               
-            }
-            
-            @Override
-            public void onActivate(DeploymentEvent event) {              
-            }
-        });
-        invoker.start();        
+        };
+        countDownListener.setDeploy(true);
+        ((ListenerSupport)deploymentService).addListener(countDownListener);
+        invoker.start();
         
         countDownListener.waitTillCompleted();
         
