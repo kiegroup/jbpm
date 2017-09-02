7f780645823cf737969353cf8b9d3dd3d7685d6b
Tihomir Surdilovic <tsurdilovic@tihomirs-mbp.attlocal.net>
2017-09-02 14:25:22 -0700

    reverting JBPM-6388

--- jbpm-workitems/jbpm-workitems-archetype/src/main/resources/archetype-resources/jbpm-workitems-__rootArtifactId__/src/main/java/__classPrefix__WorkItemHandler.java
+++ jbpm-workitems/jbpm-workitems-archetype/src/main/resources/archetype-resources/jbpm-workitems-__rootArtifactId__/src/main/java/__classPrefix__WorkItemHandler.java
@@ -1,48 +1,18 @@
 package ${package};
 
-import java.util.HashMap;
-import java.util.Map;
 import org.jbpm.process.workitem.core.AbstractLogOrThrowWorkItemHandler;
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemManager;
-import org.jbpm.process.workitem.core.util.Wid;
-import org.jbpm.process.workitem.core.util.WidParameter;
-import org.jbpm.process.workitem.core.util.WidResult;
-import org.jbpm.process.workitem.core.util.WidMavenDepends;
 
-@Wid(widfile="${classPrefix}Definitions.wid", name="${classPrefix}Definitions",
-        displayName="${classPrefix}Definitions", icon="${classPrefix}Icon.png",
-        defaultHandler="mvel: new ${package}.${classPrefix}WorkItemHandler()",
-        parameters={
-            @WidParameter(name="SampleParam"),
-            @WidParameter(name="SampleParamTwo")
-        },
-        results={
-            @WidResult(name="SampleResult")
-        },
-        mavenDepends={
-            @WidMavenDepends(group="${groupId}", artifact="${rootArtifactId}", version="${version}")
-        })
 public class ${classPrefix}WorkItemHandler extends AbstractLogOrThrowWorkItemHandler {
-
     public void executeWorkItem(WorkItem workItem,
                                 WorkItemManager manager) {
         try {
 
-            // sample parameters
-            String sampleParam = (String) workItem.getParameter("SampleParam");
-            String sampleParamTwo = (String) workItem.getParameter("SampleParamTwo");
+            // handler impl here
 
-            // complete workitem impl...
-
-
-            // return results
-            String sampleResult = "sample result";
-            Map<String, Object> results = new HashMap<String, Object>();
-            results.put("SampleResult", sampleResult);
-
-
-            manager.completeWorkItem(workItem.getId(), results);
+            manager.completeWorkItem(workItem.getId(),
+                                     null);
         } catch(Throwable cause) {
             handleException(cause);
         }
@@ -54,5 +24,4 @@ public class ${classPrefix}WorkItemHandler extends AbstractLogOrThrowWorkItemHan
         // stub
     }
 }
-
 
