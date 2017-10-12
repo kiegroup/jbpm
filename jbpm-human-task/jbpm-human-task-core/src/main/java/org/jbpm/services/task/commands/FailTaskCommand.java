13ab41c745cf26d892fc4a45fac0c8de53e3e68d
Maciej Swiderski <swiderski.maciej@gmail.com>
2017-10-12 09:51:20 +0200

    RHBPMS-4995 - Delegate task API call target user verification issue in BxMS 6.4 (#1027)

--- jbpm-human-task/jbpm-human-task-core/src/main/java/org/jbpm/services/task/commands/FailTaskCommand.java
+++ jbpm-human-task/jbpm-human-task-core/src/main/java/org/jbpm/services/task/commands/FailTaskCommand.java
@@ -59,7 +59,7 @@ public class FailTaskCommand extends UserGroupCallbackTaskCommand<Void> {
 
 	public Void execute(Context cntxt) {
         TaskContext context = (TaskContext) cntxt;
-        doCallbackUserOperation(userId, context);
+        doCallbackUserOperation(userId, context, true);
         groupIds = doUserGroupCallbackOperation(userId, null, context);
         context.set("local:groups", groupIds);
     	context.getTaskInstanceService().fail(taskId, userId, data);
