2d755f253649f624a4ae92bbcfb909dd4d040f37
Edoardo Vacchi <uncommonnonsense@gmail.com>
2018-12-04 17:28:45 +0100

    wip

--- jbpm-flow/src/main/java/org/jbpm/process/weaver/AbstractWeaverService.java
+++ jbpm-flow/src/main/java/org/jbpm/process/weaver/AbstractWeaverService.java
@@ -1,26 +1,17 @@
 package org.jbpm.process.weaver;
 
-import java.util.Map;
-
 import org.drools.core.definitions.InternalKnowledgePackage;
-import org.jbpm.process.assembler.ProcessPackage;
+import org.drools.core.definitions.ProcessPackage;
 import org.kie.api.KieBase;
 import org.kie.api.definition.KiePackage;
-import org.kie.api.internal.io.ResourceTypePackage;
 import org.kie.api.internal.weaver.KieWeaverService;
-import org.kie.api.io.ResourceType;
 
 public abstract class AbstractWeaverService implements KieWeaverService<ProcessPackage> {
 
     @Override
     public void merge(KieBase kieBase, KiePackage kiePkg, ProcessPackage processPkg) {
-        Map<ResourceType, ResourceTypePackage> map = ((InternalKnowledgePackage)kiePkg).getResourceTypePackages();
-        ResourceType resourceType = processPkg.getResourceType();
-        ProcessPackage existing  = (ProcessPackage) map.get(resourceType);
-        if ( existing == null ) {
-            existing = new ProcessPackage(resourceType);
-            map.put(resourceType, existing);
-        }
+        ProcessPackage existing = ((InternalKnowledgePackage) kiePkg).getResourceTypePackages().computeIfAbsent(
+                processPkg.getResourceType(), ProcessPackage::new);
 
         existing.getRuleFlows().putAll(processPkg.getRuleFlows());
     }
