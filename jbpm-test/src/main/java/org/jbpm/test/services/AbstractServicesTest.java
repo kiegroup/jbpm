459aa755fff768c4cba369786d338bc8ea3a7fcf
mariofusco <mario.fusco@gmail.com>
2020-02-06 10:23:16 +0100

    [DROOLS-4999] make jbpm to work with the executable model

--- jbpm-test/src/main/java/org/jbpm/test/services/AbstractServicesTest.java
+++ jbpm-test/src/main/java/org/jbpm/test/services/AbstractServicesTest.java
@@ -25,7 +25,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
-import org.drools.compiler.kie.builder.impl.DrlProject;
 import org.drools.compiler.kie.builder.impl.InternalKieModule;
 import org.jbpm.runtime.manager.impl.jpa.EntityManagerFactoryManager;
 import org.jbpm.services.api.DeploymentService;
@@ -151,7 +150,7 @@ public abstract class AbstractServicesTest {
         }
 
         KieBuilder kieBuilder = ks.newKieBuilder(kfs);
-        List<Message> messages = kieBuilder.buildAll( DrlProject.class ).getResults().getMessages();
+        List<Message> messages = kieBuilder.buildAll().getResults().getMessages();
         if (!messages.isEmpty()) {
             for (Message message : messages) {
                 logger.error("Error Message: ({}) {}", message.getPath(), message.getText());
