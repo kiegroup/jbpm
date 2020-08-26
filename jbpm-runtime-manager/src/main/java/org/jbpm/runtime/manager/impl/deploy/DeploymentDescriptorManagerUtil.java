bb01c4d1229e637f86d219c068ea459407f00554
Enrique Gonzalez Martinez <egonzale@egonzale.remote.csb>
2020-08-26 16:38:10 +0200

    [JBPM-9310] Warning in BAMTaskEventListener if task is skipped from task event listener

--- jbpm-runtime-manager/src/main/java/org/jbpm/runtime/manager/impl/deploy/DeploymentDescriptorManagerUtil.java
+++ jbpm-runtime-manager/src/main/java/org/jbpm/runtime/manager/impl/deploy/DeploymentDescriptorManagerUtil.java
@@ -15,6 +15,9 @@
  */
 package org.jbpm.runtime.manager.impl.deploy;
 
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -26,6 +29,7 @@ import org.drools.compiler.kie.builder.impl.KieContainerImpl;
 import org.drools.compiler.kie.builder.impl.KieModuleKieProject;
 import org.kie.api.runtime.KieContainer;
 import org.kie.internal.runtime.conf.DeploymentDescriptor;
+import org.kie.internal.runtime.conf.MergeMode;
 import org.kie.internal.runtime.manager.deploy.DeploymentDescriptorIO;
 import org.kie.internal.runtime.manager.deploy.DeploymentDescriptorManager;
 import org.slf4j.Logger;
@@ -34,6 +38,30 @@ import org.slf4j.LoggerFactory;
 public class DeploymentDescriptorManagerUtil {
 
     private static final Logger logger = LoggerFactory.getLogger(DeploymentDescriptorManagerUtil.class);
+
+    /**
+     * This creates a deployment descriptor
+     * @param manager
+     * @param kieContainer
+     * @param presets
+     * @return
+     */
+    public static DeploymentDescriptor getDeploymentDescriptor(DeploymentDescriptorManager manager, KieContainer kieContainer, MergeMode mode, DeploymentDescriptor ...presets) {
+        List<DeploymentDescriptor> descriptorHierarchy = new ArrayList<DeploymentDescriptor>();
+        
+
+        if(presets != null) {
+            descriptorHierarchy.addAll(stream(presets).filter(e -> e != null).collect(toList()));
+        }
+
+        if (((KieContainerImpl)kieContainer).getKieProject() instanceof KieModuleKieProject) {
+            InternalKieModule module = ((KieModuleKieProject) ((KieContainerImpl)kieContainer).getKieProject()).getInternalKieModule();
+            collectDeploymentDescriptors(module, descriptorHierarchy);
+        }
+
+        descriptorHierarchy.add(manager.getDefaultDescriptor());
+        return new DeploymentDescriptorMerger().merge(descriptorHierarchy, mode);
+    }
 
     public static List<DeploymentDescriptor> getDeploymentDescriptorHierarchy(DeploymentDescriptorManager manager, KieContainer kieContainer) {
         List<DeploymentDescriptor> descriptorHierarchy = new ArrayList<DeploymentDescriptor>();
