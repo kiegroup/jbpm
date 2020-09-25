98a96b6f3166ba85b303f4318a46871ed4d4ee55
krisv <kris.verlaenen@gmail.com>
2020-09-25 02:48:00 +0200

    RHPAM-3188: Exception after dynamically adding new node to process instance

--- jbpm-bpmn2/src/test/java/org/jbpm/bpmn2/persistence/DynamicProcessTest.java
+++ jbpm-bpmn2/src/test/java/org/jbpm/bpmn2/persistence/DynamicProcessTest.java
@@ -16,16 +16,23 @@
 
 package org.jbpm.bpmn2.persistence;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.drools.core.command.impl.CommandBasedStatefulKnowledgeSession;
 import org.jbpm.bpmn2.JbpmBpmn2TestCase;
+import org.jbpm.bpmn2.xml.XmlBPMNProcessDumper;
 import org.jbpm.compiler.xml.XmlRuleFlowProcessDumper;
 import org.jbpm.persistence.session.objects.TestWorkItemHandler;
 import org.jbpm.process.instance.impl.ProcessInstanceImpl;
 import org.jbpm.ruleflow.core.RuleFlowProcess;
 import org.jbpm.ruleflow.core.RuleFlowProcessFactory;
+import org.jbpm.workflow.core.DroolsAction;
 import org.jbpm.workflow.core.impl.ConnectionImpl;
+import org.jbpm.workflow.core.impl.DroolsConsequenceAction;
 import org.jbpm.workflow.core.impl.NodeImpl;
 import org.jbpm.workflow.core.node.HumanTaskNode;
+import org.jbpm.workflow.core.node.Split;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.kie.api.KieBase;
@@ -37,6 +44,7 @@ import org.kie.api.event.process.ProcessNodeLeftEvent;
 import org.kie.api.event.process.ProcessNodeTriggeredEvent;
 import org.kie.api.event.process.ProcessStartedEvent;
 import org.kie.api.event.process.ProcessVariableChangedEvent;
+import org.kie.api.fluent.Dialect;
 import org.kie.api.io.Resource;
 import org.kie.api.runtime.Context;
 import org.kie.api.runtime.KieSession;
@@ -68,18 +76,26 @@ public class DynamicProcessTest extends JbpmBpmn2TestCase {
 			.name("HelloWorldProcess")
 			.version("1.0")
 			.packageName("org.jbpm")
+			.imports("org.jbpm.bpmn2.objects.Person")
 			// Nodes
 			.startNode(1).name("Start").done()
 			.humanTaskNode(2).name("Task1").actorId("krisv").taskName("MyTask").done()
-			.endNode(3).name("End").done()
+			.splitNode(3).name("Split").type(Split.TYPE_XOR)
+				.constraint(4, "c1", "code", Dialect.JAVA, "return new Person().getName() == null;", 1)
+				.constraint(5, "c2", "code", Dialect.JAVA, "return false;", 2)
+				.done()
+			.endNode(4).name("End1").done()
+			.endNode(5).name("End2").done()
 			// Connections
 			.connection(1, 2)
-			.connection(2, 3);
+			.connection(2, 3)
+			.connection(3, 4)
+			.connection(3, 5);
 		final RuleFlowProcess process = factory.validate().getProcess();
 		Resource resource = ResourceFactory
                 .newByteArrayResource(XmlRuleFlowProcessDumper.INSTANCE.dump(
                         process).getBytes());
-        resource.setSourcePath("/tmp/dynamicProcess.bpmn2"); // source path or target path must be set to be added into kbase
+		resource.setSourcePath("/tmp/dynamicProcess.bpmn2"); // source path or target path must be set to be added into kbase
         KieBase kbase = createKnowledgeBaseFromResources(resource);
 		StatefulKnowledgeSession ksession = createKnowledgeSession(kbase);
 		TestWorkItemHandler testHandler = new TestWorkItemHandler();
@@ -116,8 +132,12 @@ public class DynamicProcessTest extends JbpmBpmn2TestCase {
 		
 		HumanTaskNode node = new HumanTaskNode();
 		node.setName("Task2");
-		node.setId(4);
+		node.setId(6);
+		List<DroolsAction> actions = new ArrayList<DroolsAction>();
+		actions.add(new DroolsConsequenceAction("java", "System.out.println(\"OnEntry\");"));
+		node.setActions("onEntry", actions);
 		insertNodeInBetween(process, 2, 3, node);
+		XmlBPMNProcessDumper.INSTANCE.dump(process);
 		
 		((CommandBasedStatefulKnowledgeSession) ksession).getRunner().execute(new ExecutableCommand<Void>() {
 			public Void execute(Context context) {
