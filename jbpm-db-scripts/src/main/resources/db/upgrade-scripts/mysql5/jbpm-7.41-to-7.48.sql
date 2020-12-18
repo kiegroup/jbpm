d735cf40db9f4bad1b8c9a881d4d893488219daf
afalhambra <antferna@redhat.com>
2020-12-18 15:52:26 +0100

    [JBPM-9514] - New CorrelationKeyInfo constraint upgrade in DDL script is failing for DB2 databases

--- jbpm-db-scripts/src/main/resources/db/upgrade-scripts/mysql5/jbpm-7.41-to-7.48.sql
+++ jbpm-db-scripts/src/main/resources/db/upgrade-scripts/mysql5/jbpm-7.41-to-7.48.sql
@@ -1,2 +1,2 @@
-alter table CorrelationKeyInfo alter column name set not null;
+alter table CorrelationKeyInfo modify name varchar(255) not null;
 alter table CorrelationKeyInfo add constraint IDX_CorrelationKeyInfo_name unique (name);
