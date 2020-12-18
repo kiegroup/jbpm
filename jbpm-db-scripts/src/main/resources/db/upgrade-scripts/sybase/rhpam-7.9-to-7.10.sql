ab9047e6140a219723038ed25a1f55dd0a5eb454
afalhambra <antferna@redhat.com>
2020-12-18 13:25:07 +0100

    [JBPM-9514] - New CorrelationKeyInfo constraint upgrade in DDL script is failing for DB2 databases

--- jbpm-db-scripts/src/main/resources/db/upgrade-scripts/sybase/rhpam-7.9-to-7.10.sql
+++ jbpm-db-scripts/src/main/resources/db/upgrade-scripts/sybase/rhpam-7.9-to-7.10.sql
@@ -1,4 +1,4 @@
-alter table CorrelationKeyInfo alter column "name" not null
+alter table CorrelationKeyInfo alter column name set not null
 go
 alter table CorrelationKeyInfo add constraint IDX_CorrelationKeyInfo_name unique (name)
 go
