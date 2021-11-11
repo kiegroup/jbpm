104d71695224e4730c1f97f3306f66c2b4f958f8
Marián Macik <mmacik@redhat.com>
2021-11-11 12:00:43 +0100

    JBPM-9944 - 'Cannot find the object "TimerMappingInfo"' thrown on Microsoft SQL

--- jbpm-db-scripts/src/main/resources/db/ddl-scripts/sqlserver/sqlserver-jbpm-schema.sql
+++ jbpm-db-scripts/src/main/resources/db/ddl-scripts/sqlserver/sqlserver-jbpm-schema.sql
@@ -101,7 +101,7 @@
     );
 
     create table TimerMappingInfo (
-    	id bigint numeric(19,0) identity not null, 
+    	id numeric(19,0) identity not null,
     	externalTimerId varchar(255), 
     	kieSessionId numeric(19,0) not null, 
     	timerId numeric(19,0) not null, 
