fc4cebe861c2512bb193779498b912549183b3b4
afalhambra <antferna@redhat.com>
2022-06-27 13:17:13 +0200

    [JBPM-10087] - DDL scripts fix

--- jbpm-db-scripts/src/main/resources/db/upgrade-scripts/sqlserver/rhpam-7.13-to-7.13.1.sql
+++ jbpm-db-scripts/src/main/resources/db/upgrade-scripts/sqlserver/rhpam-7.13-to-7.13.1.sql
@@ -1,4 +1,4 @@
 ALTER TABLE TimerMappingInfo ADD processInstanceId numeric(19,0);
-ALTER TABLE TimerMappingInfo ALTER timerId numeric(19,0);
+ALTER TABLE TimerMappingInfo ALTER COLUMN timerId numeric(19,0) NULL;
 DROP INDEX EventTypes.IDX_EventTypes_element;
 CREATE INDEX IDX_EventTypes_IdElement ON EventTypes(InstanceId,element);
