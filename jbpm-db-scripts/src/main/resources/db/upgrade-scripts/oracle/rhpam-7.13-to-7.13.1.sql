49953a69e3a9a3bb9b4c52f4d4e39600b4032c54
afalhambra <antferna@redhat.com>
2022-06-27 13:17:13 +0200

    [JBPM-10087] - DDL scripts fix

--- jbpm-db-scripts/src/main/resources/db/upgrade-scripts/oracle/rhpam-7.13-to-7.13.1.sql
+++ jbpm-db-scripts/src/main/resources/db/upgrade-scripts/oracle/rhpam-7.13-to-7.13.1.sql
@@ -1,2 +1,4 @@
 ALTER TABLE TimerMappingInfo ADD processInstanceId number(19,0);
-ALTER TABLE TimerMappingInfo ALTER COLUMN timerId number(19,0);
+ALTER TABLE TimerMappingInfo MODIFY (timerId NULL);
+DROP INDEX IDX_EventTypes_element;
+CREATE INDEX IDX_EventTypes_IdElement ON EventTypes(InstanceId,element);
--- jbpm-db-scripts/src/main/resources/db/upgrade-scripts/postgresql/rhpam-7.13-to-7.13.1.sql
+++ jbpm-db-scripts/src/main/resources/db/upgrade-scripts/postgresql/rhpam-7.13-to-7.13.1.sql
@@ -1,2 +1,4 @@
 ALTER TABLE TimerMappingInfo ADD COLUMN processInstanceId int8;
-ALTER TABLE TimerMappingInfo ALTER COLUMN timerId int8;
+ALTER TABLE TimerMappingInfo ALTER COLUMN timerId DROP NOT NULL;
+DROP INDEX IDX_EventTypes_element;
+CREATE INDEX IDX_EventTypes_IdElement ON EventTypes(InstanceId,element);
