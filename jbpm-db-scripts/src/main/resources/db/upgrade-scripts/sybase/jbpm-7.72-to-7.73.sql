768c2c48bd81915df869ce2fdb4c25a5067577c6
VaniHaripriya <89943213+VaniHaripriya@users.noreply.github.com>
2022-08-10 01:55:39 -0500

    [7.67.x-blue]..[JBPM-10094]- Current index settings might cause DeadLocks in SQL Server (#2177)
    * [7.67.x]..[JBPM-10094]- Current index settings might cause DeadLocks in SQL Server (#2163)
    
    * [JBPM-10053]-UserTaskInstanceWithCustomVarsQueryMapper not returning "COLUMN_TASK_SLA_DUE_DATE" (#2135)
    
    * [JBPM-10053]-UserTaskInstanceWithCustomVarsQueryMapper not returning COLUMN_TASK_SLA_DUE_DATE
    
    * [JBPM-10053]-UserTaskInstanceWithCustomVarsQueryMapper not returning COLUMN_TASK_SLA_DUE_DATE
    
    * [JBPM-10053] - Added the missing attributes to UserTaskInstanceWithCustomVarsQueryMapper
    
    * [JBPM-10053]-Added test case for UserTaskInstanceWithCustomVarsQueryMapper
    
    * [JBPM-10053]- Updated assert statement
    
    * [JBPM-10053]- Updated missing attributes
    
    * [JBPM-10053]- Reverted to the previous version of UserTaskInstanceDesc
    
    * [7.67.x]..[JBPM-10094]- Current index settings might cause DeadLocks in SQL Server (#2160)
    
    * [JBPM-10094]- Current index settings might cause DeadLocks in SQL Server
    
    * [JBPM-10094]- Updated sqlserver2008 script
    
    * [JBPM-10094] Current index settings might cause DeadLocks in SQL Server
    
    * [JBPM-10094]- Updated the index for all the dbs
    
    * Update jbpm-db-scripts/src/main/resources/db/upgrade-scripts/oracle/jbpm-7.72-to-7.73.sql
    
    Co-authored-by: afalhambra <antferna@redhat.com>
    
    * Update jbpm-db-scripts/src/main/resources/db/upgrade-scripts/oracle/rhpam-7.13-to-7.13.1.sql
    
    Co-authored-by: afalhambra <antferna@redhat.com>
    
    * Update jbpm-db-scripts/src/main/resources/db/upgrade-scripts/postgresql/jbpm-7.72-to-7.73.sql
    
    Co-authored-by: afalhambra <antferna@redhat.com>
    
    * Update jbpm-db-scripts/src/main/resources/db/upgrade-scripts/postgresql/rhpam-7.13-to-7.13.1.sql
    
    Co-authored-by: afalhambra <antferna@redhat.com>
    
    * Update jbpm-db-scripts/src/main/resources/db/upgrade-scripts/h2/jbpm-7.72-to-7.73.sql
    
    Co-authored-by: afalhambra <antferna@redhat.com>
    
    * Update jbpm-db-scripts/src/main/resources/db/upgrade-scripts/h2/rhpam-7.13-to-7.13.1.sql
    
    Co-authored-by: afalhambra <antferna@redhat.com>
    
    * [JBPM-10094]- Updating scripts in ddl60
    
    Co-authored-by: afalhambra <antferna@redhat.com>
    
    Co-authored-by: afalhambra <antferna@redhat.com>
    
    * Removed extra spaces
    
    Co-authored-by: afalhambra <antferna@redhat.com>

--- jbpm-db-scripts/src/main/resources/db/upgrade-scripts/sybase/jbpm-7.72-to-7.73.sql
+++ jbpm-db-scripts/src/main/resources/db/upgrade-scripts/sybase/jbpm-7.72-to-7.73.sql
@@ -1,3 +1,4 @@
-ALTER TABLE TimerMappingInfo ADD COLUMN info blob(2147483647);
 DROP INDEX EventTypes.IDX_EventTypes_element;
+go
 CREATE INDEX IDX_EventTypes_IdElement ON EventTypes(InstanceId,element);
+go
