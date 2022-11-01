b2d62a369e387bef89f598d423b15e4f8a6843f0
Antonio Fernandez Alhambra <antferna@redhat.com>
2022-11-01 12:54:44 +0100

    [7.67.x-blue][JBPM-10094] - Fix wrong alter statements for some databases (#2220)

--- jbpm-db-scripts/src/main/resources/db/upgrade-scripts/sqlserver2008/rhpam-7.13-to-7.13.1.sql
+++ jbpm-db-scripts/src/main/resources/db/upgrade-scripts/sqlserver2008/rhpam-7.13-to-7.13.1.sql
@@ -1,5 +1,4 @@
 ALTER TABLE TimerMappingInfo ADD processInstanceId bigint;
-ALTER TABLE TimerMappingInfo ALTER timerId bigint;
 ALTER TABLE TimerMappingInfo ALTER COLUMN timerId bigint NULL;
 DROP INDEX EventTypes.IDX_EventTypes_element;
 CREATE INDEX IDX_EventTypes_IdElement ON EventTypes(InstanceId,element);
